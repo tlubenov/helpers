b7bae4e498b7bc002a8fe67205cffb62bf77396a
Todor Lubenov <tlubenov@gmail.com>
2021-07-23 17:10:41 +0300

    add scripts

---
+++ init_python.sh
@@ -0,0 +1,21 @@
+
+# init venv and libs
+
+sudo apt-get update
+sudo apt-get upgrade
+
+python3 -m venv venv
+source venv/bin/activate
+
+which python
+
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install scipy
+pip install yellowbrick
+
+
+
+
+
---
+++ postgresql.sh
@@ -0,0 +1,11 @@
+
+
+
+# https://www.postgresqltutorial.com/postgresql-backup-database/
+# https://www.tecmint.com/backup-and-restore-postgresql-database/
+
+# Restore given database with user gis
+pg_restore -U gis -d test vrc_08_20210721.tar
+
+
+
